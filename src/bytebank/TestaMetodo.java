0ff3aea80ef15be026af398c03fb95b787a529a9
Kaue Bachiega <kauebachiega@gmail.com>
2023-01-26 23:17:18 -0300

    Java orientado a Objeto com Alura

---
+++ src/bytebank/TestaMetodo.java
@@ -0,0 +1,26 @@
+package bytebank;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar= contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaMarcela = new Conta();
+		contaMarcela.deposita(1000);
+		
+		if(contaMarcela.transfere(300, contaDoPaulo)) {
+			System.out.println("Transferencia com sucesso");
+		} else {
+			System.out.println("faltou dinheiro");
+		}
+		System.out.println(contaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+	}
+	
+}
